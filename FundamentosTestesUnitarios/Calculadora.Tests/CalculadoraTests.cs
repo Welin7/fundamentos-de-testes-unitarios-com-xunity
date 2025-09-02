79f33ceaa9b221cdf8d8f98c8d5e36b385bf997a
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-09-02 00:27:46 -0300

    Criado solution com os projetos.
    Criado projeto principal da calculadora e o projeto de testes. Implementado o método de somar números positivos que retorna a soma exata.

---
+++ FundamentosTestesUnitarios/Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Somar_Numeros_Positivos_Deve_Retornar_Soma_Exata()
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+            int numeroUm = 8;
+            int numeroDois = 4;
+            int resultadoDaSomaEsperado = 12;
+            
+            //Act
+            var resultaDaSoma = calculadora.Somar(numeroUm, numeroDois);
+            
+            //Assert
+            Assert.Equal(resultadoDaSomaEsperado, resultaDaSoma);
+
+        }
+    }
+}
---
+++ FundamentosTestesUnitarios/Calculadora/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    {
+        public int Somar(int numeroUm, int numeroDois)
+        {
+            return numeroUm + numeroDois;
+        }
+    }
+}
