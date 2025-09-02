9431138dc92c31246d3a7b216fe917dde4e9e9f3
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-09-02 01:01:17 -0300

    Criado teste para várias somas.
    Implementado o método de somar para muitos números usando theory e inlinedata.

--- FundamentosTestesUnitarios/Calculadora.Tests/CalculadoraTests.cs
+++ FundamentosTestesUnitarios/Calculadora.Tests/CalculadoraTests.cs
@@ -2,7 +2,7 @@
 {
     public class CalculadoraTests
     {
-        [Fact]
+        [Fact(DisplayName ="Somar_Numeros_Positivos_Soma_Exata")]
         public void Somar_Numeros_Positivos_Deve_Retornar_Soma_Exata()
         {
             //Arrange
@@ -17,6 +17,24 @@
             //Assert
             Assert.Equal(resultadoDaSomaEsperado, resultaDaSoma);
 
+        }
+
+        [Theory]
+        [InlineData(8, 4, 12)]
+        [InlineData(15, 5, 20)]
+        [InlineData(20, 30, 50)]
+        [InlineData(100, 200, 300)]
+        [InlineData(50, 60, 100)]
+        public void SomarMuitosNumerosPositivosDeveRetornarSomaExata(int numeroUm, int numeroDois, int resultadoDaSomaEsperado)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+            
+            //Act
+            var resultaDaSoma = calculadora.Somar(numeroUm, numeroDois);
+
+            //Assert
+            Assert.Equal(resultadoDaSomaEsperado, resultaDaSoma);
         }
     }
 }
